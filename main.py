c7fecfb1ccd609229b34d75a53277f3cc3593f03
Yashraj <yashrajnimbalkar1323@gmail.com>
2026-09-07 16:56:20 +0530

    just checking

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routes import employee_router, salary_router
 
-app = FastAPI(title="emp mng api")
+app = FastAPI(title="employee management api")
 
 app.include_router(employee_router)
 app.include_router(salary_router)
